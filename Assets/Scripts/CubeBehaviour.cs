ce82492afb4ba998e2a94db2830ed0284626eef5
unknown <stefan_stafie@yahoo.comgit config --global user.name StefanStafie>
2020-10-03 14:06:56 +0300

    Signed-off-by: unknown <stefan_stafie@yahoo.comgit config --global user.name StefanStafie>

---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+
+    public GameObject cube;
+    private Renderer cubeRenderer;
+    void Start()
+    {
+        cubeRenderer = cube.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cube.transform.position.y<-0.4f)
+            cubeRenderer.material.SetColor("_Color", Color.red);
+        else
+            cubeRenderer.material.SetColor("_Color", Color.blue);
+    }
+}
---
+++ Assets/Scripts/SphereBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehaviour : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject imageTarget;
+    private Vector3 location;
+    void Start()
+    {
+        location = new Vector3(1,1,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(imageTarget.transform.position.y>-0.3f)
+        {
+            location = location + new Vector3(0.01f,0.01f,0.01f);
+        }
+        else{
+            location = new Vector3(1f,1f,1f);
+        }
+        sphere.transform.localScale = location;
+    }
+}
